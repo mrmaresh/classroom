275f0a4640f65c6e514cff2118bce2c43ff55350
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-05-17 16:46:29 +0000

    Tue, May 17, 2022, 4:46 PM +00:00

--- attendance/admin.py
+++ attendance/admin.py
@@ -6,9 +6,12 @@ from .models import Student, Record
 class StudentAdmin (ImportExportMixin, admin.ModelAdmin):
     list_display = ("first", "last", "student_id", "period", "id")
 
+class RecordAdmin (admin.ModelAdmin):
+    list_display = ("student_id", "timestamp", "reason", "id")
+
+
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Record, RecordAdmin)
-
 
 
