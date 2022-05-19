5e9f51354a13c6905e24e0996c5520db4c20eb0a
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-05-19 03:35:43 +0000

    Thu, May 19, 2022, 3:35 AM +00:00

--- attendance/views.py
+++ attendance/views.py
@@ -14,11 +14,11 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Student, Record
 
 def login(request):
-    time_threshold = datetime.now() - timedelta(hours=1)
+    time_threshold = datetime.now() - timedelta(hours=6)
     record_query = Record.objects.filter(timestamp__gt=time_threshold)
     test = Record.objects.all().order_by('-timestamp')[0].timestamp
     return render(request, "login.html",{
-        "restroom": datetime.now().hour == test.hour - 7 + 24,
+        "restroom": record_query,
         "test": time_threshold
     })
 
