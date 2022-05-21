3150ed6eaf3a6e0bb4ab50caab408b826b413b9b
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-05-21 05:20:15 +0000

    Sat, May 21, 2022, 5:20 AM +00:00

--- attendance/urls.py
+++ attendance/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 path("", views.login, name="login"),
 path("record", views.record, name="record"),
+path("waitlist", views.waitlist, name="waitlist"),
 
 
 # API Routes
