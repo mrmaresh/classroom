da6602f354ba1736d7525a023219dad22119d430
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-05-26 20:46:39 +0000

    Thu, May 26, 2022, 8:46 PM +00:00

--- attendance/views.py
+++ attendance/views.py
@@ -16,21 +16,18 @@ from .models import Student, Record, Bathroom, Waitlist
 
 schedules = {
     "regular": [ time(6,45,0,0), time(7,55,0,0), time(8,57,0,0), time(10,3,0,0), time(11,5,0,0), time(12,0,0,0), time(12,45,0,0), time(13,46,0,0), time(14,40,0,0)],
-    "even":    [time(7,20,0,0), time(7,20,0,0), time(8,7,0,0), time(8,7,0,0), time(10,8,0,0), time(12,2,0,0), time(12,47,0,0), time(12,47,0,0), time(14,40,0,0)],
-    "odd": [time(7,20,0,0), time(8,7,0,0), timetime(10,8,0,0), time(12,2,0,0), time(12,47,0,0), time(14,40,0,0)]
+    "even": [time(7,20,0,0), time(7,20,0,0), time(8,7,0,0), time(8,7,0,0), time(10,8,0,0), time(12,2,0,0), time(12,47,0,0), time(12,47,0,0), time(14,40,0,0)],
+    "odd": [time(7,20,0,0), time(8,7,0,0), time(10,8,0,0), time(10,8,0,0), time(12,2,0,0), time(12,2,0,0), time(12,47,0,0), time(12,47,0,0), time(14,40,0,0)]
 }
 
 def get_current_period(schedule):
     now = datetime.now()
-    if schedule in ["odd"]:
-        list = [0,1,3,5,7,9]
-        r = 6
-    for i in range(r):
+    for i in range(9):
         hour = schedules[schedule][i].hour
         minute = schedules[schedule][i].minute
         then = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
         if now < then:
-            return list[i]
+            return i
     return 0
 
 
