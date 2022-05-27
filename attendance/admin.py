575bbb59164f06d0b20044460cfba8da6d2baac0
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-05-27 16:51:28 +0000

    Fri, May 27, 2022, 4:51 PM +00:00

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from import_export.admin import ImportExportMixin
 
-from .models import Student, Record, Bathroom, Waitlist
+from .models import Student, Record, Bathroom, Waitlist, Schedule
 
 class StudentAdmin (ImportExportMixin, admin.ModelAdmin):
     list_display = ("first", "last", "student_id", "period", "exception", "id")
@@ -15,11 +15,15 @@ class BathroomAdmin (ImportExportMixin, admin.ModelAdmin):
 class WaitlistAdmin (admin.ModelAdmin):
     list_display = ("student_id", "id")
 
+class ScheduleAdmin (admin.ModelAdmin):
+    list_display = ("schedule", "active", "period_0", "period_1", "period_2", "period_3", "period_4", "period_5", "period_6", "period_7", "period_8",)
+
 
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Record, RecordAdmin)
 admin.site.register(Bathroom, BathroomAdmin)
 admin.site.register(Waitlist, WaitlistAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
 
 
