467974727649c9d8d204f8d1d45661b20208c7fb
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-05-27 19:33:34 +0000

    Fri, May 27, 2022, 7:33 PM +00:00

--- attendance/views.py
+++ attendance/views.py
@@ -50,7 +50,7 @@ def login(request):
 
     if len(record_query) > 0:
         for record in record_query:
-            student = Student.objects.get(pk = record['student_id'])
+            student = Student.objects.get(student_id = record['student_id'])
             records = Record.objects.filter(student=student, timestamp__range = [start,finish]).order_by('-timestamp')
             if records[0].reason == "use_restroom":
                 name = Student.objects.get(student_id = record['student_id']).first
