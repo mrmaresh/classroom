75c15b9f3344065fc180a7867ee8767934d77f9b
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-05-30 20:34:19 +0000

    Mon, May 30, 2022, 8:34 PM +00:00

--- attendance/views.py
+++ attendance/views.py
@@ -81,7 +81,7 @@ def login(request):
         "period": get_current_period(),
         "start": start,
         "finish": finish,
-        "student_query": "hello"
+        "student_query": datetime.strptime(my_date_string, '%b %d %Y %I:%M%p')
     })
 
 
