899414acaf32d8438009251e5ba115df98253b7d
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-06-02 17:49:04 +0000

    Thu, Jun 2, 2022, 5:49 PM +00:00

--- attendance/static/function.js
+++ attendance/static/function.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
     window.onload = displayClock();
     function displayClock(){
         var time = new Date().toLocaleTimeString();
-        document.getElementById('txt').innerHTML = time;
+        var date = new Date().toDateString();
+        document.getElementById('txt').innerHTML = time ;
         setTimeout(displayClock, 1000);
     }
 
