316897d3e61c83e9206a827840f2f7b9a1b99360
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-06-02 05:24:03 +0000

    Thu, Jun 2, 2022, 5:24 AM +00:00

--- attendance/views.py
+++ attendance/views.py
@@ -76,7 +76,7 @@ def login(request):
         "waitlist": Waitlist.objects.all(),
         "waiting": waiting,
         "records": Record.objects.filter(timestamp__range = [start,finish]),
-        "period": get_current_period()[0],
+        "period": get_current_period()[0][-1],
         "start": start,
         "finish": finish,
         "schedule": Schedule.objects.get(active = True).schedule_name
