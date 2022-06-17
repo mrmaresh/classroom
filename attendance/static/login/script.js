7329634958caeed90b19094dec5fbf89f81978c1
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-06-17 05:17:22 +0000

    Fri, Jun 17, 2022, 5:17 AM +00:00

---
+++ attendance/static/login/script.js
@@ -0,0 +1,18 @@
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter,idx) => `<span style="transition-delay:${idx * 70}ms">${letter}</span>`)
+        .join('')
+});
+
+
+    window.onload = displayClock();
+    function displayClock(){
+        var time = new Date().toLocaleTimeString();
+        var date = new Date().toDateString();
+        document.querySelector('.time').innerHTML = time;
+        document.querySelector('.date').innerHTML = date;
+        setTimeout(displayClock, 1000);
+    }
