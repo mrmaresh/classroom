f291a25f0e3f936fd5632c4e89e67964e9dacf02
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-06-22 19:39:14 +0000

    Wed, Jun 22, 2022, 7:39 PM +00:00

--- attendance/static/select/script.js
+++ attendance/static/select/script.js
@@ -1,6 +1,7 @@
 const optionButton = document.querySelector('.option-button');
+const restroomButton = document.querySelector('.option-restroom');
 
 optionButton.addEventListener('click', () => {
-    console.log("hello");
     optionButton.classList.add('click');
+    restroomButton.classList.add('click');
 });
