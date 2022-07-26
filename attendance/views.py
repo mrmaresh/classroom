3091ddb35c865e932e5459a2f6595699d6cfe12a
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-07-26 04:36:12 +0000

    Tue, Jul 26, 2022, 4:36 AM +00:00

--- attendance/views.py
+++ attendance/views.py
@@ -221,7 +221,7 @@ def dashboard(request):
     startdate = datetime.today()-timedelta(hours=8)
     records = Bathroom.objects.filter(time_out__gt = startdate).order_by('-time_out')
     return render(request, 'dashboard.html',{
-        "timeSpent": start_time,
+        "timeSpent": start_time.hours,
         "waitlist": Waitlist.objects.all(),
         "records": records,
         "startdate":startdate.date,
