964ff307923b26acd43b0847221a135c5332816f
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-07-26 00:45:04 +0000

    Tue, Jul 26, 2022, 12:45 AM +00:00

--- attendance/views.py
+++ attendance/views.py
@@ -146,6 +146,7 @@ def select(request):
         records = Record.objects.filter(student=student, timestamp__range = [start,finish]).order_by('-timestamp')
         returning = is_returning(records)
         return render(request, 'select.html',{
+            "test": Student.objects.filter(student=student)
             "month": datetime.now().month,
             "numPass": numPass(student),
             "student": student,
