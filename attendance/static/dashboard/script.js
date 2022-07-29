a2c96e28b9f0a62d5670692fae8701b4048ba0b3
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-07-29 22:12:55 +0000

    Fri, Jul 29, 2022, 10:12 PM +00:00

--- attendance/static/dashboard/script.js
+++ attendance/static/dashboard/script.js
@@ -1,3 +1,12 @@
 $(document).ready( function () {
     $('#filter-table').DataTable();
-    } );
+} );
+
+window.onload = displayClock();
+function displayClock(){
+    var time = new Date().toLocaleTimeString();
+    var date = new Date().toDateString();
+    document.querySelector('.time').innerHTML = time;
+    document.querySelector('.date').innerHTML = date;
+    setTimeout(displayClock, 1000);
+}
