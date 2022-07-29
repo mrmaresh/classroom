ab79931afc30b12814a77355cbd1ba3c84826a49
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-07-29 22:35:50 +0000

    Fri, Jul 29, 2022, 10:35 PM +00:00

--- attendance/static/dashboard/script.js
+++ attendance/static/dashboard/script.js
@@ -20,7 +20,7 @@ function setTime() {
 
     const ampm = hours < 12 ? 'AM' : 'PM';
 
-    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes}:${minutes < 10 ? `0 ${seconds}` : seconds} ${ampm}`;
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0 ${seconds}` : seconds} ${ampm}`;
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
 }
 
