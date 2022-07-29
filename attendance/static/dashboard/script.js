e8a8098d5f51b9b056f6ff6875a514541f0de413
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-07-29 22:26:23 +0000

    Fri, Jul 29, 2022, 10:26 PM +00:00

--- attendance/static/dashboard/script.js
+++ attendance/static/dashboard/script.js
@@ -2,11 +2,30 @@ $(document).ready( function () {
     $('#filter-table').DataTable();
 } );
 
-time = document.querySelector('#time');
-date = document.querySelector('#date');
+timeEl = document.querySelector('#time');
+dateEl = document.querySelector('#date');
 
-time.innerText = "3:45";
-date.innerHTML = "MY DATE";
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hours = time.getHours();
+    const hoursForClock = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    const ampm = hours < 12 ? 'AM' : 'PM';
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${(360/12)*hours}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${(360/60)*minutes}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${(360/60)*seconds}deg)`;
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+setInterval(setTime, 1000);
 
 
 
