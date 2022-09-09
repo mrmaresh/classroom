34afa65bf339aa3032c0709bf37286829146c069
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-09-09 20:00:22 +0000

    Fri, Sep 9, 2022, 8:00 PM +00:00

--- attendance/models.py
+++ attendance/models.py
@@ -36,13 +36,13 @@ class AttendanceRecord (models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name = "studentAttendanceRecord")
     timestamp = models.DateTimeField(auto_now_add=True)
     excused = models.BooleanField()
-    reason = models.CharField(max_length=20)
+    reason = models.CharField(max_length=200)
 
 class Incident (models.Model):
     id = models.AutoField(primary_key=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name = "studentIncident")
     timestamp = models.DateTimeField(auto_now_add=True)
-    reason = models.CharField(max_length=200)
+    reason = models.CharField(max_length=20)
 
 class Bathroom (models.Model):
     id = models.AutoField(primary_key=True)
