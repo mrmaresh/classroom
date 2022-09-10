d5f4819999bba538106a0012140a6de6aacc49e1
Jason Maresh <85368513+JasonMaresh@users.noreply.github.com>
2022-09-10 22:38:35 +0000

    LJ

--- attendance/models.py
+++ attendance/models.py
@@ -8,7 +8,7 @@ class Student (models.Model):
     last = models.CharField(max_length=20)
     student_id = models.CharField(max_length=10)
     period = models.CharField(max_length=1)
-    grade = models.CharField(max_length = 2)
+    grade = models.CharField(max_length = 2, default = '9')
     exception = models.BooleanField()
     augPass = models.BooleanField(default=True)
     sepPass = models.BooleanField(default=True)
